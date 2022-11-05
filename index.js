1c8de5b9304cf3f55e206e4da5b27af627da8d09
Ashish <115717632+ashish-54321@users.noreply.github.com>
2022-11-05 16:11:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 
 const http = require('http').createServer(app)
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 5000
 
 
 http.listen(PORT, () => {
